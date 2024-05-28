4513000ae982df40e4e62ccae6f5462c0b0c63af
Peter Gallwas [peg0252] (they/them) <peg0252@arastudent.ac.nz>
2024-05-28 14:13:24 +1200

    remove var from bat

--- opensite.bat
+++ opensite.bat
@@ -1,8 +1,5 @@
 @ECHO OFF
 :: Start simple http server with a couple arguments to make it work with the site
 :: Simple http server can be found here https://github.com/TheWaWaR/simple-http-server
-SET /p file_path=Where is the website on your drive:
-SET /p server_port=What port do you want to run the server on?:
-
-START ./httpserver.exe -i --nocache --cors --ip 127.0.0.1 -p %server_port% %file_path%
+START ./httpserver.exe -i --nocache --cors --ip 127.0.0.1 -p 80 ./
 
